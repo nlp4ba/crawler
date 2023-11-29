b70acc77411bae4265690519029cca019b6dd3eb
Martin Hoffmann <71267302+martinh-dev@users.noreply.github.com>
2023-11-29 15:26:42 +0100

    optimazation

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 const axios = require("axios");
 const cheerio = require('cheerio');
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('./master_db/recipes_' + new Date() + '.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the database.');
+
+    db.serialize(() => {
+        db.run("CREATE TABLE recipes (link TEXT, duration TEXT, difficulty TEXT, ingredients TEXT, description TEXT)");
+    });
+
+    getAllCategories().then(async (categoriesDOM) => {
+        const categories = categoriesDOM('.category-column > div > a');
+        //slice(32)
+        for (const element of categories){
+            const attributes = element.attribs;
+            let link = attributes['href'];
+           
+            console.log("Categorie", link);
+    
+            await getLinksToRecipesByPageNumber('https://www.chefkoch.de' + link);
+        }
+    
+        db.close((err) => {
+            if (err) {
+              console.error(err.message);
+            }
+            console.log('Close the database connection.');
+          });
+    
+        });
+
+});
 
 async function getAllCategories() {
-    var response = await axios.get("https://www.chefkoch.de/rezepte/kategorien/").catch((err) => console.log(err));
-    if(response.status !== 200){
-        console.log("Error occurred while fetching data");
+    try {
+        var response = await axios.get("https://www.chefkoch.de/rezepte/kategorien/").catch((err) => console.log(err));
+        if(response.status !== 200){
+            console.log("Error occurred while fetching data");
+            return;
+        }
+        var html = response.data;
+        var dom = cheerio.load(html);
+        return dom;
+    }catch (e) {
+        console.log("Fetching categories failed");
         return;
     }
-    var html = response.data;
-    var dom = cheerio.load(html);
-    return dom;
 }
 
 async function getLinksToRecipesByPageNumber(link) {
@@ -48,13 +87,37 @@ async function getLinksToRecipesByPageNumber(link) {
 
             var html = response.data;
             var dom = cheerio.load(html);
-            const receipes = dom('.ds-recipe-card__link');
+            const receipes = dom('div:not([data-vars-payed-content-type="plus_recipe"]) > .ds-recipe-card__link');
             for (const recipe of receipes){
                 await new Promise(async (resolve, reject) => {
                 var recipeAttributes = recipe.attribs;
                 let link = recipeAttributes['href'];
 
-                var recipeContentResult = await axios.get(link);
+                function sleep(ms) {
+                    return new Promise((resolve) => {
+                      setTimeout(resolve, ms);
+                    });
+                  }
+
+                const requestReceipeData = async () => {
+                    try {
+                        var recipeContentResult = await axios.get(link);
+                        return recipeContentResult;
+                    }
+                    catch (err) {
+                        console.log("Recipe Request failed, wait for 10 seconds");
+                        await sleep(10000);
+                        return false;
+                    }
+                }
+                
+                var recipeContentResult = await requestReceipeData();
+                recipeContentResult == false ? recipeContentResult = await requestReceipeData() : recipeContentResult;
+
+                if(recipeContentResult == false){
+                    return;
+                }
+                
                 var recipeConentHTML = recipeContentResult.data;
                 var recipeContent = cheerio.load(recipeConentHTML);
 
@@ -80,7 +143,9 @@ async function getLinksToRecipesByPageNumber(link) {
                 var description = recipeContent('body > main > article.ds-box.ds-grid-float.ds-col-12.ds-col-m-8.ds-or-3 > div:nth-child(3)').text();
                 var fullRecipeData = {link: link, duration: duration, difficulty: difficulty, ingredients: ingredients.join("; "), description: description.replace(/(\r\n|\n|\r)/gm, "").trim()};
                 allRecipies.push(fullRecipeData);
+                //Write to Database
                 console.log(fullRecipeData);
+                db.run("INSERT INTO recipes(link, duration, difficulty, ingredients, description) VALUES (?, ?, ?, ?, ?)", [link, duration, difficulty, ingredients.join("; "), description.replace(/(\r\n|\n|\r)/gm, "").trim()]);
                 resolve();
                 });
             }
@@ -92,17 +157,3 @@ async function getLinksToRecipesByPageNumber(link) {
 
         return allRecipies;
 }
-
-getAllCategories().then(async (categoriesDOM) => {
-    const categories = categoriesDOM('.category-column > div > a');
-    for (const element of categories){
-        const attributes = element.attribs;
-        let link = attributes['href'];
-       
-        console.log("Categorie", link);
-
-        var test = await getLinksToRecipesByPageNumber('https://www.chefkoch.de' + link);
-        console.log(test);
-    }
-
-    });
